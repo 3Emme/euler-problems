6697ce905b041de8c21881bce275fabe6a1b000a
3Emme <emmettbuentiempo@gmail.com>
2020-09-01 15:52:57 -0700

    first pass at Euler1.prototype.findMult.
    Co-authored-by: Ben Stoller <stollersystemsound@gmail.com>

--- src/euler1.js
+++ src/euler1.js
@@ -1,7 +1,8 @@
 export default function Euler1(multArray,maxNum) {
   this.multArray = multArray
   this.maxNum = maxNum
-  this.numArray = []
+  this.numArray = []  
+  this.finalArray = []
 }
 
   Euler1.prototype.createArray = function() {
@@ -9,4 +10,16 @@ export default function Euler1(multArray,maxNum) {
       this.numArray.push(i);
     } 
     return this.numArray
+  }
+
+
+  Euler1.prototype.findMult = function() {
+    this.multArray.forEach(function(multiple){
+      this.numArray.forEach(function(number){
+        if (number % multiple === 0)
+          this.finalArray.push(number)
+      })
+    })
+    console.log(this.finalArray.length)
+    return this.finalArray    
   }
