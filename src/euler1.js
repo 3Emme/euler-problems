1f71555065aae8aaa2e0e3d0523a14fa451157b5
3Emme <emmettbuentiempo@gmail.com>
2020-09-01 15:00:25 -0700

    create Euler object and pass test 'should create an object with two parameters'.
    Co-authored-by: Ben Stoller <stollersystemsound@gmail.com>

---
+++ src/euler1.js
@@ -0,0 +1,12 @@
+export default function Euler1(multArray,maxNum) {
+  this.multArray = multArray
+  this.maxNum = maxNum
+  this.numArray = []
+}
+
+  // Euler1.prototype.createArray = function() {
+  //   for (let i = 0;i<this.maxNum;i++) {
+  //     this.numArray.push(i);
+  //   } 
+  //   return this.numArray
+  // }
