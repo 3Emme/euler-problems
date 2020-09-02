baec3b08058bd33e03724e68bdc7923f40fef16b
3Emme <emmettbuentiempo@gmail.com>
2020-09-01 21:30:48 -0700

    make a fourth pass at Euler1.prototype.findMult, passing test 3.
    Co-authored-by: Ben Stoller <stollersystemsound@gmail.com>

--- src/euler1.js
+++ src/euler1.js
@@ -6,7 +6,7 @@ export default function Euler1(multArray,maxNum) {
 }
 
   Euler1.prototype.createArray = function() {
-    for (let i = 0;i<this.maxNum;i++) {
+    for (let i = 1;i<this.maxNum;i++) {
       this.numArray.push(i);
     } 
     return this.numArray
